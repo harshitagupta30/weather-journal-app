0dcffb65532922cc9ec5ae557e25d6da21e66366
harshitag <harshita.gupta@zooplus.com>
2020-05-24 15:45:19 +0200

    Updated the styles and added check for the fields

--- website/app.js
+++ website/app.js
@@ -6,8 +6,11 @@ const apiKey = 'ae684c8809f260571a8216177ef33e56'; // Personal API Key for OpenW
 let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 
+const userInfo = document.getElementById('userInfo');
+
 // Event listener to add function to existing HTML DOM element
-document.getElementById('generate').addEventListener('click', performAction);
+const generateBtn = document.getElementById('generate');
+generateBtn.addEventListener('click', performAction);
 
 /* Function called by event listener */
 function performAction(e) {
@@ -17,14 +20,26 @@ function performAction(e) {
     const zipCode = document.getElementById('zip').value;
     const content = document.getElementById('feelings').value;
 
-    getWeatherData(baseUrl, zipCode, apiKey)
-        .then(function(data) {
-            // add data to POST request
-            postData('/add', { temp: data.main.temp, date: newDate, content: content });
-        }).then(function() {
-            // call updateUI to update browser content
-            updateUI()
-        });
+    if (zipCode !== '') {
+        generateBtn.classList.remove('invalid');
+        getWeatherData(baseUrl, zipCode, apiKey)
+            .then(function(data) {
+                // add data to POST request
+                postData('/add', { temp: convertKelvinToCelsius(data.main.temp), date: newDate, content: content });
+            }).then(function() {
+                // call updateUI to update browser content
+                updateUI()
+            }).catch(function(error) {
+                console.log(error);
+                alert('The zip code is invalid. Try again');
+
+            });
+        userInfo.reset();
+    } else {
+        generateBtn.classList.add('invalid');
+    }
+
+
 }
 
 /* Function to GET Web API Data*/
@@ -69,10 +84,21 @@ const updateUI = async() => {
         const allData = await request.json();
         console.log(allData);
         // update new entry values
-        document.getElementById('date').innerHTML = allData.date;
-        document.getElementById('temp').innerHTML = allData.temp;
-        document.getElementById('content').innerHTML = allData.content;
+        if (allData.date !== undefined && allData.temp !== undefined && allData.content !== undefined) {
+            document.getElementById('date').innerHTML = allData.date;
+            document.getElementById('temp').innerHTML = allData.temp + ' degree C';
+            document.getElementById('content').innerHTML = allData.content;
+        }
     } catch (error) {
         console.log('error', error);
     }
 };
+
+// helper function to convert temperature from Kelvin to Celsius
+function convertKelvinToCelsius(kelvin) {
+    if (kelvin < (0)) {
+        return 'below absolute zero (0 K)';
+    } else {
+        return (kelvin - 273.15).toFixed(2);
+    }
+}
